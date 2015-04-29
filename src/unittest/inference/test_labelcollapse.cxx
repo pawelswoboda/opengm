c60d921ea5559fa6f3082b4436d26833b6fe8d05
Stefan Haller <stefan.haller@tu-dresden.de>
2015-04-29 14:54:45 +0100

    Updates test suite for LabelCollapse.
    Most of the comments and machinery were out of date.

--- src/unittest/inference/test_labelcollapse.cxx
+++ src/unittest/inference/test_labelcollapse.cxx
@@ -16,43 +16,33 @@
 
 int main()
 {
+	srand(time(0));
+
 	typedef opengm::GraphicalModel<double, opengm::Adder> OriginalModelType;
 	typedef opengm::LabelCollapseAuxTypeGen<OriginalModelType>::GraphicalModelType AuxiliaryModelType;
 	typedef opengm::BlackBoxTestGrid<OriginalModelType> GridTest;
 	typedef opengm::BlackBoxTestFull<OriginalModelType> FullTest;
 	typedef opengm::BlackBoxTestStar<OriginalModelType> StarTest;
 
-	// We really need more than two states per variable, otherwise no labels
-	// can get collapsed.
-	opengm::InferenceBlackBoxTester<OriginalModelType> allTester, bruteforceTester;
+	opengm::InferenceBlackBoxTester<OriginalModelType> tester;
 
-	// At first some rather small amount of variables (chain like) with
-	// different settings and increasing state space.
-	//
-	// The first pass is run with Bruteforce as proxy inference and the second
-	// pass uses CombiLP. CombiLP does not handle problems without unary terms.
-	// So these problems will only be used for the first pass. (Problems
-	// without a unary term are a good test case, because most of the bugs were
-	// found using this kind of problem and they are simpler to debug.)
-	allTester.addTest(new GridTest(1,  2,   4, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 1000));
-	allTester.addTest(new GridTest(1,  2,   8, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 100));
-	allTester.addTest(new GridTest(1,  2, 100,  true,  true, GridTest::RANDOM, opengm::OPTIMAL, 3));
-	allTester.addTest(new GridTest(1, 10,   3, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
-	allTester.addTest(new GridTest(1,  5,   8, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 1));
+	tester.addTest(new GridTest(1,  4,  4, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 2000));
+	tester.addTest(new GridTest(1,  4,  4, false, false, GridTest::RANDOM, opengm::OPTIMAL, 2000));
+	tester.addTest(new GridTest(1,  4,  8, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 100));
+	tester.addTest(new GridTest(1,  4,  8, false, false, GridTest::RANDOM, opengm::OPTIMAL, 100));
+	tester.addTest(new GridTest(1,  4, 20,  true,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
+	tester.addTest(new GridTest(1,  4, 20, false, false, GridTest::RANDOM, opengm::OPTIMAL, 10));
 
-	bruteforceTester.addTest(new GridTest(1,  2,   4, false, false, GridTest::RANDOM, opengm::OPTIMAL, 1000));
-	bruteforceTester.addTest(new GridTest(1,  2,   8, false, false, GridTest::RANDOM, opengm::OPTIMAL, 100));
-	bruteforceTester.addTest(new GridTest(1,  2, 100, false, false, GridTest::RANDOM, opengm::OPTIMAL, 30));
+	tester.addTest(new GridTest(1, 10,  3, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
+	tester.addTest(new GridTest(1,  5,  8, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
 
-	// Now some more variables, but less states and iterations. This is very
-	// time consuming to brute force. :-(
-	allTester.addTest(new GridTest(2,  2,   6, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 20));
-	allTester.addTest(new StarTest(5,       6, false,  true, StarTest::RANDOM, opengm::OPTIMAL, 20));
+	tester.addTest(new GridTest(3,  3,  5, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 5));
+	tester.addTest(new GridTest(3,  3,  5, false, false, GridTest::RANDOM, opengm::OPTIMAL, 5));
 
-	bruteforceTester.addTest(new GridTest(2,  2,   6, false, false, GridTest::RANDOM, opengm::OPTIMAL, 20));
-	bruteforceTester.addTest(new StarTest(5,       6, false, false, StarTest::RANDOM, opengm::OPTIMAL, 20));
-	bruteforceTester.addTest(new FullTest(3,       6, false,     2, FullTest::RANDOM, opengm::OPTIMAL, 20));
-	// Note above: FullTest also uses no unary terms. Does not work with CombiLP.
+	tester.addTest(new StarTest(5,      6, false,  true, StarTest::RANDOM, opengm::OPTIMAL, 20));
+	tester.addTest(new StarTest(5,      6, false, false, StarTest::RANDOM, opengm::OPTIMAL, 20));
+
+	tester.addTest(new FullTest(3,      6, false,     2, FullTest::RANDOM, opengm::OPTIMAL, 20));
 
 	std::cout << "Test LabelCollapse ..." << std::endl;
 
@@ -61,8 +51,7 @@ int main()
 		typedef opengm::Bruteforce<AuxiliaryModelType, opengm::Minimizer> Proxy;
 		typedef opengm::LabelCollapse<OriginalModelType, Proxy> Inference;
 		Inference::Parameter parameter;
-		allTester.test<Inference>(parameter);
-		bruteforceTester.test<Inference>(parameter);
+		tester.test<Inference>(parameter);
 	}
 
 #ifdef WITH_CPLEX
@@ -75,8 +64,7 @@ int main()
 		proxy_parameter.integerConstraint_ = true;
 
 		Inference::Parameter parameter(proxy_parameter);
-		allTester.test<Inference>(parameter);
-		bruteforceTester.test<Inference>(parameter);
+		tester.test<Inference>(parameter);
 	}
 #endif
 }
