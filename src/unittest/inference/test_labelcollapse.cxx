0ca22ad5fc10cfa332d8e3fd069d4ed1396efee8
Stefan Haller <stefan.haller@tu-dresden.de>
2015-05-05 17:10:45 +0100

    Disables tests without unary potentials for LabelCollapse.
    The new LabelCollapse implementation uses unaries potentials to
    maintain a stack of collapsed labels.
    
    It won’t work with models without unary potentials.

--- src/unittest/inference/test_labelcollapse.cxx
+++ src/unittest/inference/test_labelcollapse.cxx
@@ -27,22 +27,12 @@ int main()
 	opengm::InferenceBlackBoxTester<OriginalModelType> tester;
 
 	tester.addTest(new GridTest(1,  4,  4, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 2000));
-	tester.addTest(new GridTest(1,  4,  4, false, false, GridTest::RANDOM, opengm::OPTIMAL, 2000));
 	tester.addTest(new GridTest(1,  4,  8, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 100));
-	tester.addTest(new GridTest(1,  4,  8, false, false, GridTest::RANDOM, opengm::OPTIMAL, 100));
 	tester.addTest(new GridTest(1,  4, 20,  true,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
-	tester.addTest(new GridTest(1,  4, 20, false, false, GridTest::RANDOM, opengm::OPTIMAL, 10));
-
 	tester.addTest(new GridTest(1, 10,  3, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
 	tester.addTest(new GridTest(1,  5,  8, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 10));
-
 	tester.addTest(new GridTest(3,  3,  5, false,  true, GridTest::RANDOM, opengm::OPTIMAL, 5));
-	tester.addTest(new GridTest(3,  3,  5, false, false, GridTest::RANDOM, opengm::OPTIMAL, 5));
-
 	tester.addTest(new StarTest(5,      6, false,  true, StarTest::RANDOM, opengm::OPTIMAL, 20));
-	tester.addTest(new StarTest(5,      6, false, false, StarTest::RANDOM, opengm::OPTIMAL, 20));
-
-	tester.addTest(new FullTest(3,      6, false,     2, FullTest::RANDOM, opengm::OPTIMAL, 20));
 
 	std::cout << "Test LabelCollapse ..." << std::endl;
 
